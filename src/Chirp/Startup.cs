6d01364cbbc93c2b04de96705ab66eba0a6622c3
Stephen Fallon <elitefusion92@gmail.com>
2015-11-29 22:44:44 -0500

    html-css: Static index.html file.

--- src/Chirp/Startup.cs
+++ src/Chirp/Startup.cs
@@ -20,12 +20,7 @@ namespace Chirp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
         }
 
         // Entry point for the application.
