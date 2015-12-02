66e6deff6403b1e70d26a3e4a77dfd91a5a95cd3
Stephen Fallon <elitefusion92@gmail.com>
2015-12-01 22:53:20 -0500

    EF7: Created ChirpMessage class.

---
+++ src/Chirp/Models/ChirpMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chirp.Models
+{
+    public class ChirpMessage
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime PostTime { get; set; }
+        public int UserId { get; set; }
+    }
+}
