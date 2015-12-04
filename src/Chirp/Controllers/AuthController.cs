f1dec85729cbc5734640f167656d457082404043
Stephen Fallon <elitefusion92@gmail.com>
2015-12-04 00:59:28 -0500

    identity: Login View Page

---
+++ src/Chirp/Controllers/AuthController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Chirp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Chirps", "App");
+            }
+
+            return View();
+        }
+    }
+}
