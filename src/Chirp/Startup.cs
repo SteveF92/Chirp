4d05196af0bb0317f85c51c0d62da8b6a9c52d63
Stephen Fallon <elitefusion92@gmail.com>
2015-12-05 01:22:43 -0500

    identity: Using Identity in the API

--- src/Chirp/Startup.cs
+++ src/Chirp/Startup.cs
@@ -15,6 +15,8 @@ using AutoMapper;
 using Chirp.ViewModels;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Authentication.Cookies;
+using System.Net;
 
 namespace Chirp
 {
@@ -51,6 +53,21 @@ namespace Chirp
                 config.User.RequireUniqueEmail = true;
                 config.Password.RequiredLength = 8;
                 config.Cookies.ApplicationCookie.LoginPath = "/Auth/Login";
+                config.Cookies.ApplicationCookie.Events = new CookieAuthenticationEvents()
+                {
+                    OnRedirectToLogin = ctx =>
+                    {
+                        if (ctx.Request.Path.StartsWithSegments("/api") && ctx.Response.StatusCode == (int) HttpStatusCode.OK)
+                        {
+                            ctx.Response.StatusCode = (int) HttpStatusCode.Unauthorized;
+                        }
+                        else
+                        {
+                            ctx.Response.Redirect(ctx.RedirectUri);
+                        }   
+                        return Task.FromResult(0);
+                    }
+                };
             })
             .AddEntityFrameworkStores<ChirpContext>();
 
