ba49d7697d93dbeec3bd53101533bc56a084ea42
Stephen Fallon <elitefusion92@gmail.com>
2016-02-06 13:47:16 -0500

    signalr: About page SignalR test.

---
+++ src/Chirp/Hubs/ChirpPostHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Chirp.Hubs
+{
+    public class ChirpPostHub : Hub
+    {
+        public void RefreshChirps(string receivedString)
+        {
+            var responseString = string.Empty;
+
+            responseString = "";
+            Clients.All.refreshChirps(responseString);
+        }
+    }
+}
---
+++ src/Chirp/wwwroot/js/about/application.js
@@ -0,0 +1,11 @@
+﻿var applicationModule = angular.module('application', []);
+
+// SignalR's hub object.
+var chirpPostHub = $.connection.chirpPostHub;
+
+$(function () {
+    $.connection.hub.logging = true;
+    $.connection.hub.start();
+});
+
+angular.module('application').value('chirpPostHub', chirpPostHub);
