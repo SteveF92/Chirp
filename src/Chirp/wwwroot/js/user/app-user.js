35b0bc9745c14c6c9a9e9a142b66734122d1759f
Stephen Fallon <elitefusion92@gmail.com>
2016-02-26 00:00:07 -0500

    users-page: Moved users page to angular

---
+++ src/Chirp/wwwroot/js/user/app-user.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+    //Creating the module
+    angular.module("app-user", []);
+
+    // SignalR's hub object.
+    var chirpPostHub = $.connection.chirpPostHub;
+
+    $(function () {
+        $.connection.hub.logging = true;
+        $.connection.hub.start();
+    });
+
+    angular.module('app-user').value('chirpPostHub', chirpPostHub);
+})();
---
+++ src/Chirp/wwwroot/js/user/userController.js
@@ -0,0 +1,59 @@
+﻿(function () {
+    "use strict";
+
+    //Getting the existing module
+    angular.module("app-user")
+        .controller("userController", userController);
+
+    function userController($http, $scope, chirpPostHub) {
+        var vm = this;
+        vm.user = {};
+        vm.pageUserName = pageUserName;
+        vm.chirpPosts = [];
+        vm.errorMessage = "";
+
+        vm.getUser = function () {
+            vm.isBusy = true;
+            var userUrl = "/api/user/";
+            userUrl = userUrl.concat(vm.pageUserName);
+            $http.get(userUrl)
+            .then(function (response) {
+                //Success
+                angular.copy(response.data, vm.user);
+            }, function (error) {
+                //Failure
+                vm.errorMessage = "Failed to get User Info: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+        };
+
+        vm.getChirps = function () {
+            vm.isBusy = true;
+            var userUrl = "/api/chirpposts/";
+            userUrl = userUrl.concat(vm.pageUserName);
+            $http.get(userUrl)
+            .then(function (response) {
+                //Success
+                angular.copy(response.data, vm.chirpPosts);
+            }, function (error) {
+                //Failure
+                vm.errorMessage = "Failed to get Chirps: " + error;
+            })
+            .finally(function () {
+                vm.isBusy = false;
+            });
+        };
+
+        vm.getUser();
+        vm.getChirps();
+
+        // Method which receives data.
+        chirpPostHub.client.refreshChirps = function () {
+            // Method which handles messages.
+            vm.getChirps();
+            $scope.$apply();
+        };
+    }
+})();
