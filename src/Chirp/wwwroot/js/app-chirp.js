7d42adf1f4d2b0090128d614f9a6f5bff633f262
Stephen Fallon <elitefusion92@gmail.com>
2016-03-01 22:54:26 -0500

    layout: Split SignalR file away from app file, to be included separately.

--- src/Chirp/wwwroot/js/app-chirp.js
+++ src/Chirp/wwwroot/js/app-chirp.js
@@ -3,14 +3,4 @@
    
     //Creating the module
     angular.module("app-chirp", ["chirpControls", "simpleControls"]);
-
-    // SignalR's hub object.
-    var chirpPostHub = $.connection.chirpPostHub;
-
-    $(function () {
-        $.connection.hub.logging = true;
-        $.connection.hub.start();
-    });
-
-    angular.module('app-chirp').value('chirpPostHub', chirpPostHub);
 })();
---
+++ src/Chirp/wwwroot/js/chirpSignalR.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict";
+
+    // SignalR's hub object.
+    var chirpPostHub = $.connection.chirpPostHub;
+
+    $(function () {
+        $.connection.hub.logging = true;
+        $.connection.hub.start();
+    });
+
+    angular.module('app-chirp').value('chirpPostHub', chirpPostHub);
+})();
